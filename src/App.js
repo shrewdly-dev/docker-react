6a05ca9269389d81df9040f16721c8017618e8d2
shrewdly-dev <andrew.deakin@gmx.co.uk>
2024-04-03 17:45:14 +0100

    Update for deploy

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-         Deploy Code
+         Deploy Process Sucks
         </p>
         <a
           className="App-link"
